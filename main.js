c0fb41913fb1958277eed6655f7d3aa3b8e83a49
LeandraCodes821 <Ms.knowitall2336@gmail.com>
2022-03-21 16:52:21 -0700

    Adds variables for each coin

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 // Write your JavaScript here
-let amtDue= document.getElementById("amount-due")
-let amtReceived= document.getElementById("amount-received")
+function handleClickEvent() {
+
+    let amtDue = parseFloat(document.getElementById("amount-due").value);
+    let amtReceived= parseFloat(document.getElementById("amount-received").value);
+    let changeOwed= (amtReceived - amtDue);
+    
 
 
-const handleClickEvent= e => {
-    //console.log(amtReceived.value)
-}
+
+    let wholeDollars = Math.floor(changeOwed/1);
+    changeOwed= changeOwed%1;
+    document.getElementById("dollars-output").innerHTML= wholeDollars
 
 
+    let quarterValue= Math.floor(changeOwed/.25);
+    changeOwed= changeOwed%.25;
+    document.getElementById
 
-document.getElementById("calculate-change").addEventListener("click", () => handleClickEvent());
+    let dimeValue= Math.floor(changeOwed/.10);
+    changeOwed= changeOwed%.10;
+    console.log("dimeValue",dimeValue)
+
+    document.getElementById("change-returned-msg").innerHTML= `Your change is $${amtReceived - amtDue}`;
+    //console.log(amtReceived,amtDue)
+    //console.log(changeOwed)
+
+    }
+//}
+
+        
+        
+        document.getElementById("calculate-change").addEventListener("click", () => handleClickEvent());
+    
