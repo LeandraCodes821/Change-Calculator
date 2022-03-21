695370dc0991017c8926f68983f06c4bca2c7eed
LeandraCodes821 <Ms.knowitall2336@gmail.com>
2022-03-21 11:23:30 -0700

    Adds handleClickEvent

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // Write your JavaScript here
+let amtDue= document.getElementById("amount-due")
+let amtReceived= document.getElementById("amount-received")
+
+
+const handleClickEvent= e => {
+    //console.log(amtReceived.value)
+}
+
+
+
+document.getElementById("calculate-change").addEventListener("click", () => handleClickEvent());
