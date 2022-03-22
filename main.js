e8b4924ca88bb0e54c330b3775a5565e40c4948f
LeandraCodes821 <Ms.knowitall2336@gmail.com>
2022-03-22 11:50:40 -0700

    Adds .toFixed(2) to changeOwed calculation

--- main.js
+++ main.js
@@ -9,17 +9,28 @@ function handleClickEvent() {
 
 
     let wholeDollars = Math.floor(changeOwed/1);
-    changeOwed= changeOwed%1;
-    document.getElementById("dollars-output").innerHTML= wholeDollars
+    changeOwed= (changeOwed%1).toFixed(2);
+    document.getElementById("dollars-output").innerHTML= wholeDollars;
 
 
     let quarterValue= Math.floor(changeOwed/.25);
-    changeOwed= changeOwed%.25;
-    document.getElementById
+    changeOwed= (changeOwed%.25).toFixed(2);
+    document.getElementById("quarters-output").innerHTML= quarterValue;
 
     let dimeValue= Math.floor(changeOwed/.10);
-    changeOwed= changeOwed%.10;
-    console.log("dimeValue",dimeValue)
+    changeOwed= (changeOwed%.10).toFixed(2);
+    document.getElementById("dimes-output").innerHTML= dimeValue;
+
+    let nickelValue= Math.floor(changeOwed/0.05);
+    changeOwed= (changeOwed%0.05).toFixed(2);
+    document.getElementById("nickels-output").innerHTML= nickelValue;
+
+    let pennyValue= Math.floor(changeOwed/0.01);
+    changeOwed= (changeOwed%0.01).toFixed(2);
+    document.getElementById("pennies-output").innerHTML= pennyValue;
+
+
+    console.log("pennyValue",pennyValue)
 
     document.getElementById("change-returned-msg").innerHTML= `Your change is $${amtReceived - amtDue}`;
     //console.log(amtReceived,amtDue)
